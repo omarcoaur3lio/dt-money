f2d5a52d8d1d3519e676dc9775a2cb1f9cdc456f
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-03-27 21:44:02 -0300

    :sparkles: feat: Finalizar estrutura visual

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
-import logImg from '../../assets/logo.svg'
+import logImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
-  return(
+  return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
-  )
+  );
 }
