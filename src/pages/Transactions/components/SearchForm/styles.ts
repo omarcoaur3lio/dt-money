7feec1dea12894e86bc1d15d410706fbaa0e3f7c
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-04-12 22:48:48 -0300

    feat: adicionar react hook form nos formulários da aplicação

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -33,7 +33,7 @@ export const SearchFormContainer = styled.form`
 
     transition: background-color 0.2s, color 0.2s, border-color 0.2s;
 
-    &:hover {
+    &:not(:disabled):hover {
       background: ${(props) => props.theme["green-500"]};
       border-color: ${(props) => props.theme["green-500"]};
       color: ${(props) => props.theme.white};
